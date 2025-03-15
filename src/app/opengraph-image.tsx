836fa745502263c9b61f8eb5903a73d49ec4be7c
maheshmuttintidev <maheshmuttintidev@gmail.com>
2025-03-15 15:45:12 +0530

    update og

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,34 +1,59 @@
-import { ImageResponse }  from "next/og";
+import { ImageResponse } from 'next/og';
 
+export const alt = 'How to Behave with Others';
+export const size = { width: 1200, height: 630 };
+export const contentType = 'image/png';
 
+import { Roboto } from 'next/font/google';
 
-export default function handler() {
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+export default function Image() {
   return new ImageResponse(
     (
       <div
+        className={roboto.className}
         style={{
-          display: "flex",
-          height: "100%",
-          width: "100%",
-          backgroundColor: "#f0f4f8",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-          fontFamily:
-            'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          textAlign: 'center',
         }}
       >
-        <h1 style={{ fontSize: 60, color: "#007BFF", marginBottom: 20 }}>
+        <img
+          src="https://i.ibb.co/xqH1sRWd/icon.png"
+          alt="Website Icon"
+          style={{
+            width: '200px',
+            height: '200px', // Adjust height as needed
+            borderRadius: '50%',
+            marginBottom: '20px', // Space between image and text
+          }}
+        />
+        <div
+          style={{
+            fontSize: 100,
+            color: '#000',
+          }}
+        >
           How to Behave with Others
-        </h1>
-        <p style={{ fontSize: 30, color: "#333" }}>
+        </div>
+        <p style={{ fontSize: 42, maxWidth: '80%' }}>
           Bible-Based Guidance for Every Situation
         </p>
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      ...size,
     }
   );
 }
+
+
